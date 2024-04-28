4a481b43786b0f092237be4a4639f140dfacc989
01ou <141899225+01ou@users.noreply.github.com>
2024-04-29 08:37:01 +0900

    add tailwindTypes

---
+++ src/types/tailwindTypes.ts
@@ -0,0 +1,27 @@
+export type TailwindColor =
+  | 'gray'
+  | 'red'
+  | 'yellow'
+  | 'green'
+  | 'blue'
+  | 'indigo'
+  | 'purple'
+  | 'pink'
+  | 'white'
+  | 'black';
+
+export type TailwindSize =
+  | 'xs'
+  | 'sm'
+  | 'base'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | '3xl'
+  | '4xl'
+  | '5xl'
+  | '6xl'
+  | '7xl'
+  | '8xl'
+  | '9xl'
+  
